5e9766cda2b09975dddd332d656f40064ade2b21
Walter Magni <waltermagni@terra.com.br>
2024-06-13 20:21:41 -0300

    Script Python que gera dados aleatórios no Django com o Faker

--- contact/models.py
+++ contact/models.py
@@ -1,12 +1,18 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Category(models.Model):
+
   name = models.CharField(max_length=30)
   
   def __str__(self) -> str:
     return self.name
+  
+  class Meta:
+    verbose_name = 'Category'
+    verbose_name_plural = 'Categories'
 
 class Contact(models.Model):
   first_name = models.CharField(max_length=30)
@@ -18,6 +24,8 @@ class Contact(models.Model):
   show = models.BooleanField(default=True)
   picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
   category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True)
+  owner = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True)
+
 
   def __str__(self) -> str:
     return f'{self.first_name} {self.last_name}'
