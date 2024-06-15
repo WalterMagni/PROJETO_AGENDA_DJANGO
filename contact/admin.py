2cf841fa3cdf5763fc3d65d0a3aa74e5ca0bf1c7
Walter Magni <waltermagni@terra.com.br>
2024-06-15 12:54:18 -0300

    Criando o cabeçalho principal do site (header)

--- contact/admin.py
+++ contact/admin.py
@@ -4,13 +4,13 @@ from contact import models
 @admin.register(models.Contact)
 # Register your models here.
 class ContactAdmin(admin.ModelAdmin):
-  list_display = 'id',  'first_name', 'last_name', 'phone', 
+  list_display = 'id',  'first_name', 'last_name', 'phone', 'show'
   ordering = 'id',
   #list_filter = 'created_date',
   search_fields = 'id', 'first_name', 'last_name',
   list_per_page = 30
   list_max_show_all= 200
-  #list_editable = 'first_name', 'last_name'
+  list_editable =  'show', #'first_name', 'last_name',
   list_display_links = 'id',
 
 @admin.register(models.Category)
--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from contact.models import Contact
+from django.http import Http404
+
 
 # Create your views here.
 def index(request):
 
-  contacts = Contact.objects.all()
+  contacts = Contact.objects.filter(show=True).order_by('id')
   context = {
     'contacts': contacts
   }
@@ -12,6 +14,22 @@ def index(request):
 
   return render(
     request, 'contact/index.html',
+    context
+  )
+
+def contact(request, contact_id):
+
+  #single_contact = Contact.objects.filter(pk=contact_id).first()
+  #single_contact = get_object_or_404(Contact.objects.filter(show=True).order_by('id'), pk=contact_id)
+  single_contact = get_object_or_404(Contact, pk=contact_id)
+
+  context = {
+    'contact': single_contact
+  }
+  
+
+  return render(
+    request, 'contact/contact.html',
     context
   )
 
