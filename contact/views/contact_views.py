b3c866a2ec52cc36ac812cf38d5113418a793bf4
Walter Magni <waltermagni@terra.com.br>
2024-06-16 09:12:27 -0300

    Usando a classe Paginator para paginação no Django

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -2,21 +2,25 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.db.models import Q
 from contact.models import Contact
 from django.http import Http404
+from django.core.paginator import Paginator
 
 
 # Create your views here.
 def index(request):
 
   contacts = Contact.objects.filter(show=True).order_by('id')
+  paginator = Paginator(contacts, 10)
+  page_number = request.GET.get('page')
+  page_obj = paginator.get_page(page_number) 
+
   context = {
-    'contacts': contacts,
+    'page_obj': page_obj,
     'site_title': 'Contatos - '
   }
   
-
   return render(
     request, 'contact/index.html',
-    context
+    context,
   )
 
 def search(request):
@@ -33,9 +37,15 @@ def search(request):
                                     Q(phone__icontains=search_value) |
                                     Q(email__icontains=search_value))\
                             .order_by('id')
+  
+  paginator = Paginator(contacts, 10)
+  page_number = request.GET.get('page')
+  page_obj = paginator.get_page(page_number) 
+  
   context = {
-    'contacts': contacts,
-    'site_title': 'Contatos - '
+    'page_obj': page_obj,
+    'site_title': 'Contatos - ',
+    'search_value': search_value,
   }
   
   
