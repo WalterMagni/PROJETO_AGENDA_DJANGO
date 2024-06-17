40daf57ab8a7c9c0441625285375002442c7b461
Walter Magni <waltermagni@terra.com.br>
2024-06-17 17:32:03 -0300

    Usando instance do ModelForm para atualizar dados de um contato

--- contact/urls.py
+++ contact/urls.py
@@ -9,8 +9,9 @@ urlpatterns = [
     path('search/', views.search, name='search'),  # Corrigido 'int' para 'path'
     
     #contact (CRUD)
-    path('contact/<int:contact_id>/detail/', views.contact, name='contact'), 
+    path('contact/<int:contact_id>/detail/', views.contact, name='contact'),  
     path('contact/create/', views.create, name='create'),
+    path('contact/<int:contact_id>/update/', views.update, name='update'),
     #path('contact/<int:contact_id>/update/', views.contact, name='contact'),
     #path('contact/<int:contact_id>/delete/', views.contact, name='contact'), 
 ]
--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,42 +1,82 @@
-from django.shortcuts import render, get_object_or_404, redirect
-from django.db.models import Q
-from contact.models import Contact
-from django.http import Http404
-from django.core.paginator import Paginator
-from django.core.exceptions import ValidationError
-from contact.models import Contact
-
+from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse
 from contact.forms import ContactForm
 from contact.models import Contact
 
+
 def create(request):
+  form_action = reverse('contact:create')
+  
   if request.method == 'POST':
       
     form = ContactForm(request.POST)
 
     context = {
-        'form': form
+        'form': form,
+        'form_action': form_action,
     }
     
     if form.is_valid():
-      print('formulario é valido')
-      
-      #contact = form.save(commit=False)
-      #contact.show = False
-      #contact.save() 
-      contact = form.save()
-      print(contact)
+        print('formulario é valido')
+        
+        #contact = form.save(commit=False)
+        #contact.show = False
+        #contact.save() 
+        contact = form.save()
+        print(contact)
 
-      return redirect('contact:create')
-      #return redirect('contact:contact', contact.id)
+        #return redirect('contact:create')
+        #return redirect('contact:contact', contact.id)
+        return redirect('contact:update', contact_id=contact.pk)
 
     return render(
-        request, 'contact/create.html',
-        context,
+       request, 'contact/create.html',
+       context
     )
 
   context = {
-      'form': ContactForm()
+      'form': ContactForm(),
+      'form_action': form_action,
+  }
+
+  return render(
+      request, 'contact/create.html',
+      context,
+  )
+
+def update(request, contact_id):
+  contact = get_object_or_404(Contact, pk=contact_id, show=True)
+  form_action = reverse('contact:update', args=(contact_id,))
+  
+  if request.method == 'POST': 
+    form = ContactForm(request.POST, instance=contact)
+
+    context = {
+        'form': form,
+        'form_action': form_action,
+    }
+    
+    if form.is_valid():
+        print('formulario é valido')
+        
+        #contact = form.save(commit=False)
+        #contact.show = False
+        #contact.save() 
+        contact = form.save()
+        print(contact)
+
+        #return redirect('contact:create')
+        #return redirect('contact:contact', contact.id)
+        return redirect('contact:update', contact_id=contact.pk)
+
+    return render(
+       request, 'contact/create.html',
+       context
+    )
+
+  context = {
+      'form': ContactForm(instance=contact),
+      'form_action': form_action,
   }
 
   return render(
